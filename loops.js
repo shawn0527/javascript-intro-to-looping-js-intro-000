7f68a5333b7e89e990fc4ad8debf6fcd77a46074
Shawn Ren <doggy-ren@hotmail.com>
2019-02-01 23:56:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,14 +20,13 @@ function whileLoop (n) {
   return 'done'
 }
 
+function doWhileLoop(num){
 var i=0
-
 function incrementVariable() {
   i = i + 1;
   return i;
 }
 
-function doWhileLoop(num) {
   do {
     console.log("I run once regardless.")
   } while (incrementVariable()<num)
